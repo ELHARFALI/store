f7d65660d5d47d6a85012453a4e53a1261310417
Elharfali <elharfali.youssef.20@gmail.com>
2024-02-14 11:43:02 +0100

    Add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <main className='min-h-[calc(100vh-106px)] bg-[#FFF7F1]'>
+      <div className="main-width px-4 xl:px-2 min-h-[calc(100vh-106px)] 2xl:px-0 flex justify-center items-center ">
+        <div className=' flex justify-center items-center flex-col'>
+          <h1 className='text-3xl font-semibold md:text-4xl lg:text-5xl xl:text-6xl'>We Love <span className='px-3 lg:px-5 py-1 bg-blue-500 text-white rounded-lg'>Comfy</span></h1>
+          <p className='text-center mt-6 lg:mt-10 text-lg text-gray-500  xl:max-w-[650px]'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem totam exercitationem numquam dolor recusandae earum voluptatibus aperiam accusamus nemo eveniet nesciunt eaque commodi quis facilis modi possimus hic, doloribus iure iusto veniam sunt eligendi. Beatae?
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
